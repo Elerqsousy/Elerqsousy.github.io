0b82642f9f48cc607ddea53eec967ffc443d8bf5
Riz <mahmoud.eleresousy@gmail.com>
2024-02-25 05:52:59 +0300

    Fix footer

--- src/conponents/Footer.js
+++ src/conponents/Footer.js
@@ -8,9 +8,11 @@ const Footer = () => {
   const { setIsOpen } = React.useContext(ContactFormContext);
 
   return (
-    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light lg:text-sm fixed bottom-0'>
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light lg:text-sm bottom-0'>
       <Layout className='!py-8 flex items-center gap-3 sm:flex-col lg:!py-4 md:gap-0'>
-        <span className='felx-1 w-full md:text-center'>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+        <span className='felx-1 w-full md:text-center'>
+          {new Date().getFullYear()} &copy; All Rights Reserved
+        </span>
         <div className='flex items-center justify-center felx-1 w-full'>
           Built with <span className='text-xl px-1 md:text-base'>&#9825;</span>
           by&nbsp;
